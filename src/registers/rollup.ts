481b999854dd5553e0f0d9f613cdecf0333a1ee8
Richardo2016 <richardo2016@gmail.com>
2018-11-13 22:42:56 +0800

    [register:rollup] support emitter

--- src/registers/rollup.ts
+++ src/registers/rollup.ts
@@ -1,23 +1,25 @@
 import { setCompilerForVbox, wrapAsString } from '../vbox'
 
 import moduleList = require('@fibjs/builtin-modules')
-import { getRollupOptionsFromRegisterOptions } from './_utils';
+import { getRollupOptionsFromRegisterOptions, parseCommonOptions } from './_utils';
 
 export const SUFFIX = ['.ts', '.tsx']
 
 /**
  * @deprecated
- * @param vbox 
- * @param options 
+ * @param vbox
+ * @param options
  */
 export function _register(asPlainScript = true) {
     return function (vbox, options) {
         const util = require('util')
         const { default: rollup, plugins } = require('fib-rollup');
 
-        const {
-            compilerOptions = null, burnout_timeout = 0, suffix = SUFFIX
-        } = options || {}
+		const {
+			burnout_timeout = 0,
+			suffix = SUFFIX,
+			emitter = null
+		} = parseCommonOptions(options) || {}
 
         const rollupConfig = getRollupOptionsFromRegisterOptions(options)
 
@@ -46,7 +48,8 @@ export function _register(asPlainScript = true) {
                 return asPlainScript ? wrapAsString(rollupedJs) : rollupedJs
             },
             burnout_timeout,
-            compile_to_iife_script: !asPlainScript
+            compile_to_iife_script: !asPlainScript,
+			emitter
         })
     }
 }
